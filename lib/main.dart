fb290a3eb6df944b26446912ce6b7fc285ee84ad
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-12-21 12:41:48 -0800

    Initial commit - project setup

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_worldtime_app/pages/choose_location.dart';
+import 'package:flutter_worldtime_app/pages/loading.dart';
+import 'package:flutter_worldtime_app/pages/home.dart';
+
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
+));
+
+
